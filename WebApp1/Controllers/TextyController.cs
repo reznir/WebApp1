251c1f425ec3a5cd146bb806e67e1052d4f024ab
reznir <radek.reznicek@gmail.com>
2023-06-27 15:36:59 +0200

    Index added for TextyController.

--- WebApp1/Controllers/TextyController.cs
+++ WebApp1/Controllers/TextyController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp1.DataAccess;
 
 namespace WebApp1.Controllers
 {
     public class TextyController : Controller
     {
+        private readonly LitTextyDbContext context;
+
+        public TextyController(LitTextyDbContext Context)
+        {
+            context = Context ?? throw new ArgumentNullException();
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(context);
         }
     }
 }
--- WebApp1/wwwroot/js/site.js
+++ WebApp1/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function () {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+    });
+}
